094ea3a2d56ddc7724a8c0b8c3d9791b5286e630
AeahKa <aeah@qq.com>
2022-11-01 22:45:00 +0800

    完成Home.vue和Doc.vue的html

---
+++ src/views/Doc.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="doc">
+    <div class="content">
+      <aside>边栏</aside>
+      <main>主内容</main>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="banner">
+      <h1>V3-UI</h1>
+      <p class="action">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
