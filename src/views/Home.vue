c5eab4479d888c5d37091660bbbdc215f76f5087
AeahKa <aeah@qq.com>
2022-11-02 11:02:25 +0800

    完成主页css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="home">
-    <div class="banner">
-      <h1>V3-UI</h1>
-      <p class="action">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
-      </p>
-    </div>
+  <div class="banner">
+    <h1>V3-UI</h1>
+    <h2>一个嘴臭的框架</h2>
+    <p class="actions">
+      <a href="">GitHub</a>
+      <a href="">开始</a>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    
-  }
+export default {
+
+}
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
 
+  >.actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: calc($h/2);
+      padding: 0 8px;
+    }
+  }
+}
 </style>
