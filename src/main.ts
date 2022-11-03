7c1022045c3e3ae7d2a4702291c365d5eb81b515
AeahKa <aeah@qq.com>
2022-11-03 17:23:02 +0800

    CSS最小影响原则

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import './lib/v3.scss'
 import './index.scss'
+import App from './App.vue'
+
 import { router } from './router'
 
 const app = createApp(App)
