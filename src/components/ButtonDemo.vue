f737aa7e11079f8a7d0dd987262a7c48cd3e84ca
AeahKa <aeah@qq.com>
2022-11-03 17:23:31 +0800

    完成基础buton

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,13 +1,21 @@
 <template>
   <div>Button示例</div>
   <h1>示例1</h1>
-  <div><Button>你好</Button></div>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Button from '../lib/Button.vue';
 export default {
-  component: { Button }
+  components: { Button },
+  setup() {
+
+  }
 }
 </script>
 
