f91a19203dc4ddab99f47ba34aa0b123ee4b57f3
AeahKa <aeah@qq.com>
2022-11-03 16:35:34 +0800

    优化css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -45,14 +45,12 @@ $h2: $h - 4px;
   }
 }
 
-
-
 .vSwitch.vChecked {
   background: rgb(0, 215, 0);
-}
 
-.vSwitch.vChecked>span {
-  left: calc(100% - #{$h2} - 2px);
+  >span {
+    left: calc(100% - #{$h2} - 2px);
+  }
 }
 
 .vSwitch:focus {
