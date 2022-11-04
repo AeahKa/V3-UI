edb3a812f48ad81082dea4d2ee91fc7dab38b43e
AeahKa <aeah@qq.com>
2022-11-04 17:41:10 +0800

    Dialog支持点击关闭

--- src/components/Doc/DialogDemo.vue
+++ src/components/Doc/DialogDemo.vue
@@ -1,14 +1,26 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="visible" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
+import Button from '../../lib/Button.vue';
 import Dialog from '../../lib/Dialog.vue'
+import { ref } from 'Vue';
 export default {
   components: {
-    Dialog
+    Dialog, Button
+  },
+  setup() {
+    const visible = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    const f1 = () => { }
+    const f2 = () => { }
+    return { visible, toggle, f1, f2 }
   }
 }
 </script>
