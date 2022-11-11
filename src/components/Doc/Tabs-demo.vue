4fe084f000df9d0a486db2d6d3987a020a228901
AeahKa <aeah@qq.com>
2022-11-11 21:51:45 +0800

    改造TabsDemo

---
+++ src/components/Doc/Tabs-demo.vue
@@ -0,0 +1,23 @@
+<demo>
+常规用法
+</demo>
+
+<template>
+  <Tabs v-model:selected='x'>
+    <Tab title="导航一">内容一</Tab>
+    <Tab title="导航二">内容二</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Tab from '../../lib/Tab.vue';
+import Tabs from '../../lib/Tabs.vue';
+import { ref } from 'vue';
+export default {
+  components: { Tabs, Tab },
+  setup() {
+    const x = ref('导航一')
+    return { x }
+  }
+}
+</script>
--- src/components/Doc/TabsDemo.vue
+++ src/components/Doc/TabsDemo.vue
@@ -1,25 +1,15 @@
 <template>
-  <div>Tabs 示例</div>
-  <h1>示例1</h1>
-  <Tabs v-model:selected='x'>
-    <Tab title="导航一">内容一</Tab>
-    <Tab title="导航二">内容二</Tab>
-  </Tabs>
+  <h1>Tabs 示例</h1>
+  <Demo :component="TabsDemo"></Demo>
 </template>
 
 <script lang="ts">
-import Tab from '../../lib/Tab.vue';
-import Tabs from '../../lib/Tabs.vue';
-import { ref } from 'vue';
+import Demo from './Demo.vue';
+import TabsDemo from './Tabs-demo.vue'
 export default {
-  components: { Tabs, Tab },
+  components: { Demo },
   setup() {
-    const x = ref('导航一')
-    return { x }
+    return { TabsDemo }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
